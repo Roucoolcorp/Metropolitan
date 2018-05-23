d0dbeb6685b26578e8184f36637c4a2419139c36
Haye <alexandrehaye>
2018-05-23 15:12:14 +0200

    nouvelle structure: Reseau relié aux stations et pas aux lignes

--- fonction.c
+++ fonction.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
-void chemin_le_plus_court(station * s_depart, station * s_arrive, reseau * R)
+double recuperer_element(reseau * R, int ligne, int colonne){
+
+	int position = ligne * p_graphe->nb_colonnes+colonne;
+	int valeur = p_graphe->contenu[position];
+	return valeur;
+}
+
+int degre_positif(int s, mat_graphe *p_graphe)
 {
-	if(strcmp(s_depart->nom, s_arrive->nom) = 0)
-	{
-		printf("Vous êtes déjà arrivé\n");
-		exit(-1);
-	}
-	int i, y;
-	int tab_id[] = malloc(R->nb_station*sizeof(int));
 
-	i=0;
-	for(i=0 ; i < R->nb_ligne ; i++)
+    int degre=0;
+    int i;
+    for(i=0; i< p_graphe->nb_colonnes;i++)
+    {
+        if(recuperer_element(p_graphe,s,i)!=-1)
 	{
+            degre++;
+        }
+    }
+    return degre;
+}
 
-	}
+
+void chemin_le_plus_court(station * s_depart, reseau * R)
+{
+	station *tab_stat = (station*) malloc(R->nb_station*sizeof(station));
+    //Initialisation
+
+
+
+
 }
 
 int main()
--- struct.c
+++ struct.c
@@ -1,35 +1,40 @@
 typedef struct Station station;
 typedef struct Ligne ligne;
+//typedef struct Sous_stat sous_stat;
 
 struct Station
 {
         float lon;
-	float lat;
+        float lat;
         char * nom;
-        int suivant_aller;
-        int suivant_retour;
-	station ** suivant;
-	station ** precedent;
-	ligne ** lignes;
-        int distance;
         int id;
+        int nb_stat_adj;
+        station ** stat_adj; 
 };
 
+//struct Sous_stat
+//{
+ //       int suivant_aller;
+  //      int suivant_retour;
+    //    sous_stat ** suivants;
+      //  sous_stat ** precedent;
+       // ligne * line; 
+        //int distance;
+//};
 
-struct Ligne
-{
-        int nb_station_aller;
-        int nb_station_retour;
-	char * nom;
-        char * couleur;
-        station * stat;
-};
+//struct Ligne
+//{
+ //       int nb_station_aller;
+  //      int nb_station_retour;
+//      char * nom;
+ //       char * couleur;
+  //      station * stat;
+//};
 
 struct Reseau
 {
         int nb_station;
-        int nb_ligne;
-        ligne * line;
+        station ** stat;
 };
 
 typedef struct Reseau reseau;
